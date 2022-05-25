e788db0688d8e1da0a779390428e02e1c8472ea5
TahaOm <tahaomar001@gmail.com>
2022-05-25 11:05:39 +0100

    add controller/model for auth jwt and user

---
+++ src/api/user/user.entity.ts
@@ -0,0 +1,34 @@
+import { Exclude } from 'class-transformer';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column({ type: 'varchar' })
+  public email!: string;
+
+  @Exclude()
+  @Column({ type: 'varchar' })
+  public password!: string;
+
+  @Column({ type: 'varchar', nullable: true })
+  public name: string | null;
+
+  @Column({ type: 'timestamp', nullable: true, default: null })
+  public lastLoginAt: Date | null;
+
+  @CreateDateColumn({ name: 'created_at' })
+  public createdAt: Date | null;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+  public updatedAt: Date | null;
+}
